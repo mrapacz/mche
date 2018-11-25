833ebe605c4c7a4eb04ca951b39696d89dff0554
jakubtustanowski <tusty53@gmail.com>
2018-11-25 23:14:07 +0100

    Fibo PEPed

--- lesson3/fibo.py
+++ lesson3/fibo.py
@@ -2,19 +2,21 @@ import sys
 
 
 def fibo_easy(n):
-    a,b,c = 1,1,1
-    for i in range(2,n):
+    a, b, c = 1, 1, 1
+    for i in range(2, n):
         c = a + b
         a = b
         b = c
     return c
 
+
 def fibo_hard(n):
-    a,b = 1,1
-    for i in range(2,n):
+    a, b = 1, 1
+    for i in range(2, n):
         b = b + a
         a = b - a
     return b
+
 
 if __name__ == "__main__":
     fibo = fibo_hard
